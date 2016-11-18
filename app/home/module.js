fb28579191a238991e92ed4bd46a634a14e325ea
ZhekaVasil <zheka_1408@mail.ru>
2016-11-18 17:17:10 +0300

    add some tests

--- app/home/module.js
+++ app/home/module.js
@@ -5,10 +5,16 @@
         .controller('homeController', HomeController);
 
 
-    HomeController.$inject = [];
-    function HomeController(){
+    HomeController.$inject = ['$scope'];
+    function HomeController($scope){
         var vm = this;
         vm.name = 'Zheka';
+        vm.func = al;
+        vm.func('LoL');
+        function al(name) {
+            vm.name = name;
+            return true
+        }
     }
 
 })();
--- app/home/module.spec.js
+++ app/home/module.spec.js
@@ -1,9 +1,11 @@
 describe('Home controller', function () {
-    var $controller, HomeController;
+    var $controller, HomeController, scope;
     beforeEach(angular.mock.module('myApp.home'));
-    beforeEach(inject(function(_$controller_) {
+    beforeEach(inject(function(_$controller_, $rootScope) {
+        scope = $rootScope.$new();
         $controller = _$controller_;
-        HomeController = $controller('homeController', {});
+        HomeController = $controller('homeController', {$scope: scope});
+
     }));
 
     it('should be 4', function () {
@@ -16,5 +18,16 @@ describe('Home controller', function () {
     it('controller name should be Zheka ', function () {
         expect(HomeController.name).toEqual('Zheka');
     });
-    
+
+    it('should call function', function () {
+        var names = 'Max';
+        spyOn(HomeController, 'func').and.callThrough();
+        expect(HomeController.func).toBeDefined();
+        expect(HomeController.func).not.toHaveBeenCalled();
+        expect(HomeController.name).toEqual('Zheka');
+        HomeController.func(names);
+        expect(HomeController.func).toHaveBeenCalledWith(names);
+        expect(HomeController.name).toBe(names);
+
+    })
 });
